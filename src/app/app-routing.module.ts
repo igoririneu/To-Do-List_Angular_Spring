7cd9a53fb0659a6ab74064c272626bf189c3e1e9
IIRINEU <IIRINEU@ford.com>
2022-01-28 16:09:53 -0300

    feat(angular-material): angular material installed

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  {
+    path: '',
+    redirectTo: '/header',
+    pathMatch: 'full'
+  },
+  {
+    path: 'header', 
+    loadChildren: () => import('./modules/header/header.module').then(m => m.HeaderModule) 
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
